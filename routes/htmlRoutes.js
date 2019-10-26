904f7e5462038b1dab671c1f92d8bf3e9b383188
GDesmarais <46517644+GregoryDesmarais@users.noreply.github.com>
2019-10-26 12:54:53 -0400

    TRAVIS 9: TRAVIS: WHY?

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -14,7 +14,7 @@ module.exports = function(app) {
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(
-    dbExample
+      dbExample
       ) {
       res.render("example", {
         example: dbExample
