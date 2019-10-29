18ae74744b478269564e969868083a521dd8169a
Charles Danner <danner.ced1@gmail.com>
2019-10-28 21:38:11 -0400

    fixed bugs and updated files.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,22 +2,22 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
-  });
+  // app.get("/", function(req, res) {
+  //   db.Example.findAll({}).then(function(dbExamples) {
+  //     res.render("index", {
+  //       msg: "Welcome!",
+  //       examples: dbExamples
+  //     });
+  //   });
+  // });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
+  app.get("/example/:category", function(req, res) {
+    db.Post.findOne({ where: { id: req.params.category } }).then(function(
+      category
     ) {
-      res.render("example", {
-        example: dbExample
+      res.render("post", {
+        example: category
       });
     });
   });
