3bbb10a575cd82235176a2c2bd9e89268c6291c6
dyl trimble <dyltrimble12@gmail.com>
2019-11-04 18:03:24 -0500

    local

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -32,7 +32,7 @@ module.exports = function(app) {
         }).then(commentData => {
           userComments = commentData.length;
           const response = {
-            userName: data.userName,
+            userNa me: data.userName,
             memberSince: data.createdAt,
             // likes: data.likes,
             commentsMade: userComments,
